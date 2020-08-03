3d9d38fd679d927e896f775f2115f71752c55e97
Ryan Harrington <rharrington.web@gmail.com>
2020-08-03 13:07:57 -0600

    feat: help command

---
+++ src/config/embeds.ts
@@ -0,0 +1,13 @@
+const helpInfo = {
+    desc: 'A bot to handle TheTypingMatch community server events!',
+    descInfo: '`$help Register`\n`$help Guides`',
+    register: {
+        register: '`<tr-username>` - Make a profile using your TypeRacer account.',
+        delete: '`<discord-id>` - Delete your TypeRacer account.'
+    },
+    guides: {
+        ping: ' - Show bot latency.',
+    }
+}
+
+export { helpInfo }
