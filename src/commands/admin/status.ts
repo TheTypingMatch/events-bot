5fa9b10738073e99285ece428482c9ce06c8ea8f
Ryan Harrington <rharrington.web@gmail.com>
2020-08-13 18:06:31 -0600

    feat: status admin command

---
+++ src/commands/admin/status.ts
@@ -0,0 +1,39 @@
+import { MessageEmbed } from 'discord.js';
+import { colors, version } from '../../config/config';
+import { User } from '../../models/user.model';
+import { Tournament } from '../../models/tournament.model';
+
+const formatBracket = async bracket => {
+    const tournamentInfo = await Tournament.find();
+    const { currentRound } = tournamentInfo[0];
+
+    let description = '';
+
+    for (let user of bracket) {
+        if (user.rounds < currentRound && !description.includes(user.opponent.discordId)) {
+            description += `\n**<@${user.discordId}>** vs. **<@${user.opponent.discordId}>**`;
+        }
+    }
+
+    return description;
+}
+
+export default async (msg, client, args) => {
+    const statusEmbed = new MessageEmbed()
+        .setColor(colors.green)
+        .setAuthor('Status', msg.author.avatarURL())
+        .setTimestamp(new Date())
+        .setFooter(`EventsBot v${version}`);
+
+    const winners = await User.find({ losses: 0 });
+    const losers = await User.find({ losses: 1 });
+
+    const winnersDescription = await formatBracket(winners);
+    const losersDescription = await formatBracket(losers);
+
+    statusEmbed
+        .addField('Winners', winnersDescription || 'None')
+        .addField('Losers', losersDescription || 'None');
+
+    return msg.channel.send(statusEmbed);
+};
