698f06f31f33df147dbeb5a7eb7d01e0dde5ad5d
Ryan Harrington <rharrington.web@gmail.com>
2020-08-16 13:28:37 -0600

    feat: close command
    closes tournament registration

---
+++ src/commands/admin/close.ts
@@ -0,0 +1,15 @@
+import { MessageEmbed } from 'discord.js';
+import { colors, version } from '../../config/config';
+import { Tournament } from '../../models/tournament.model';
+
+export default async (msg, client, args) => {
+    const statusEmbed = new MessageEmbed()
+        .setColor(colors.green)
+        .setAuthor('Registration', msg.author.avatarURL())
+        .setTimestamp(new Date())
+        .setFooter(`EventsBot v${version}`);
+        .setDescription('Registration has been closed.');
+
+    Tournament.updateOne({ __v: 0 }, { isOpen: false });
+    return msg.channel.send(statusEmbed);
+};
